4ad5b6496cbddff6fa0a1f0d5d9facdfeed9c996
Theo Rakomane <t.rkomane55@gmail.com>
2023-11-01 14:52:57 +0200

    Altered src/Pages

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
-import Wackmole from "../Components/wackmole";
+import Wackmole from "../Components/Wackmole";
 
 
 const Home = () => {
